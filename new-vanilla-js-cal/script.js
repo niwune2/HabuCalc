53d48042070ed7609ccfb01a9228eee937667a25
niwune <your.email@example.com>
2023-09-02 16:37:28 +0900

    ファイルを更新しました

--- new-vanilla-js-cal/script.js
+++ new-vanilla-js-cal/script.js
@@ -1,5 +1,6 @@
     // ボタン要素を取得
     const buttons = document.querySelectorAll('button');
+    let result = document.getElementById('result');
 
     // ボタンがクリックされたときの処理を設定
     buttons.forEach(button => {
@@ -7,5 +8,7 @@
             const buttonText = button.innerText; // ボタンのテキストを取得
             console.log(`ボタンのテキストは: ${buttonText}`);
             // ここで buttonText を使用して処理を行う
+            
+            result.value = buttonText; //`result`に数字を表示する
         });
     });
