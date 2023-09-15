61fb910dca25b2aff6528ec29aaaefac3c28ebde
niwune <your.email@example.com>
2023-09-15 12:52:09 +0900

    数字ボタンを押したときに表示される数字を設定.

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ let result = document.getElementById('result');
 // const buttonStyles = buttons.dataset.numbers;
 
 //* C,CE,%,±,+,-,×,÷,=は表示する必要がない
+//* 初期の表示は`0`
 //TODO メソッド呼び出し -> オペランド(B)で表示`result.value`をリセットする
+//// クリア状態では`00`と`0`は押せないようにする
 //? numbers,operationsなどの処理は分けたほうがいい？
 
 // ボタンがクリックされたときの処理を設定
@@ -77,30 +79,49 @@ let result = document.getElementById('result');
     });
 });*/
 
+let calcHistory = [];
+result.value = '0';
 numbers.forEach(number => {
-    const numbersText = number.innerText;
     number.addEventListener('click', () => {
-        console.log(`Number:${numbersText}`);
+        const numberText = number.getAttribute('data-numbers');//押された数字
+        console.log(`Number:${numberText}`);
+        if (numberText === '00' && result.value === '0'){
+            return; // すでに '0' が表示されている場合は何もしない
+        } else if (result.value === '0' &&
+            (numberText !== '00' || numberText !== '0')) {
+            // 最初の '0' を数字に置き換え
+            result.value = numberText;
+        } else {
+            // 数字を連続して入力する
+            result.value += numberText;
+        }
     });
 });
+//TODO 小数点のあつかい
+//TODO 1. 小数点を押した時、自動的に'0.'と表示する
+//TODO 2. 2個以上の小数点を入力できないようにする
+//TODO 3. 小数点以下の桁数の制限
+//TODO 4. エラーハンドリングの実装
 
-operations.forEach(operation => {
-    const operationText = operation.innerHTML;
-    operation.addEventListener('click', () => {
-        console.log(`Operation:(${operationText})`);
+operations.forEach(operator => {
+    operator.addEventListener('click', () => {
+        console.log(`Operator:(${operator.innerHTML})`);
     });
 });
 
 clear.addEventListener('click', () => {
-    const clearText = clear.innerHTML;
-    console.log(`(${clearText}):Clear`);
+    console.log(`(${clear.innerHTML}):Clear`);
 });
 
 clearEntries.addEventListener('click', () => {
-    const ceText = clearEntries.innerHTML;
-    console.log(`(${ceText}):ClearEntries`);
+    console.log(`(${clearEntries.innerHTML}):ClearEntries`);
 });
+
 equal.addEventListener('click', () => {
-    const equalText = equal.innerHTML;
-    console.log(`(${equalText}):Equal`);
+    console.log(`(${equal.innerHTML}):Equal`);
 });
+
+//? 計算式は配列にいれるか
+//? 配列に入れるのであれば`=`->`C`の操作で配列をリセットできるようにする必要がありそう
+//? あるいは計算式ごとに配列を作るか?
+//? 計算式が何もない状態を判定する必要がありそう(もし配列が空なら...)
